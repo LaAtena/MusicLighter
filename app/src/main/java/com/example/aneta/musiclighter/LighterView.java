798c65fabdd7a636ac1ebf57e4fe5672d1c7119e
LaAtena <laatena1985@gmail.com>
2016-03-31 01:58:36 +0200

    Actually draw something
    It's a circle

--- app/src/main/java/com/example/aneta/musiclighter/LighterView.java
+++ app/src/main/java/com/example/aneta/musiclighter/LighterView.java
@@ -1,6 +1,9 @@
 package com.example.aneta.musiclighter;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.View;
 
 /**
@@ -9,5 +12,25 @@ import android.view.View;
 public class LighterView extends View {
     public LighterView(Context context) {
         super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int x = getWidth();
+        int y = getHeight();
+        int radius;
+        if (y > x) {
+            radius = x / 2;
+        } else {
+            radius = y / 2;
+        }
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.WHITE);
+        canvas.drawPaint(paint);
+        // Use Color.parseColor to define HTML colors
+        paint.setColor(Color.parseColor("#fb705d"));
+        canvas.drawCircle(x / 2, y / 2, radius, paint);
     }
 }
