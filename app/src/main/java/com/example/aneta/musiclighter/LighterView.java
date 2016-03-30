88bcbdf20de494803d3d3f5907518704a9f9aa97
LaAtena <laatena1985@gmail.com>
2016-03-31 00:15:13 +0200

    Initial commit
    Vanilla Android project

---
+++ app/src/main/java/com/example/aneta/musiclighter/LighterView.java
@@ -0,0 +1,13 @@
+package com.example.aneta.musiclighter;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Aneta on 2016-03-30.
+ */
+public class LighterView extends View {
+    public LighterView(Context context) {
+        super(context);
+    }
+}
