1f91031156ec5b4b021ae505b34219d4758bfefd
LaAtena <laatena1985@gmail.com>
2016-04-01 23:44:57 +0200

    Basic lighter look

--- app/src/main/java/com/example/aneta/musiclighter/LighterView.java
+++ app/src/main/java/com/example/aneta/musiclighter/LighterView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.view.View;
 
 /**
@@ -17,20 +18,35 @@ public class LighterView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        int x = getWidth();
-        int y = getHeight();
-        int radius;
-        if (y > x) {
-            radius = x / 2;
-        } else {
-            radius = y / 2;
-        }
+        int screenWidth = getWidth();
+        int screenHeight = getHeight();
+
+        int lighterCenterX = screenWidth/2;          // <-
+        int lighterHeight = screenHeight*2/5;
+        int lighterWidth = lighterHeight/3;
+
         Paint paint = new Paint();
         paint.setStyle(Paint.Style.FILL);
-        paint.setColor(Color.WHITE);
+        paint.setColor(Color.BLACK);
         canvas.drawPaint(paint);
         // Use Color.parseColor to define HTML colors
         paint.setColor(Color.parseColor("#fb705d"));
-        canvas.drawCircle(x / 2, y / 2, radius, paint);
+
+        int left = lighterCenterX - lighterWidth/2;
+        int top = screenHeight - lighterHeight;          // <-
+        int right = lighterCenterX + lighterWidth/2;
+        int bottom = screenHeight;
+        canvas.drawRect(left, top, right, bottom, paint);
+
+        int flameW = screenWidth/8;  // <-
+        int flameH = screenHeight/6;  // <-
+
+        int flameLeft = lighterCenterX-flameW/5;
+        int flameTop = flameH;
+        int flameRight = lighterCenterX+flameW/5;
+        int flameBottom = flameH+screenHeight/3;
+        paint.setColor(Color.parseColor("#ff8000"));
+        RectF flame = new RectF(flameLeft, flameTop, flameRight, flameBottom);
+        canvas.drawOval(flame, paint);
     }
 }
